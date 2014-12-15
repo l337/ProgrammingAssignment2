efcb6033a86141d093dfcf6e15025cfe80a0df4e
l337 <autonomousmachine@gmail.com>
2014-12-15 11:20:24 -0800

    solving an inverse matrix, using the superassignment for caching

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## makeCacheMatrix takes as input a matrix
+## first declares an m variable for storage / initialization
+## set lets us override a matrix passed into makeCacheMatrix
+## get lets us just retrieve the value of the matrix
+## getinverse and setinverse lets us retrive and set the solved values for the matrix
+## returns an accessable list to the methods if called outside the makeCacheMatrix function
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve takes a matrix created by makeCacheMatrix
+## if the inverse of the matrix hasn't been solved (in other words isn't NULL)
+## the input will *update* the inverse and store the value of m via superassignment
+## otherwise if it is not null cacheSolve will just return the inverse of the stored value
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
